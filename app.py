310e15737b296ba805ebe6e28df77db563b439f0
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2020-06-17 14:58:53 +0000

    Adding query param and request body

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from libs.cases import get_covid_cases
 from libs.timedate import getDate
@@ -10,9 +10,15 @@ app = Flask(__name__)
 @app.route("/api/v1", methods=["GET"])
 @app.route("/api/v1/<date>", methods=["GET"])
 def index(date=None):
+    query_date = request.args.get("date")
+    body_data = request.json
+
     if date is None:
-        yesterday = getDate()
-        cases = get_covid_cases(yesterday)
+        if query_date is not None:
+            d = query_date
+        else:
+            d = getDate()
+        cases = get_covid_cases(d)
     else:
         cases = get_covid_cases(date)
 
